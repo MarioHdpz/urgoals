7d03112ddc738c4c89e76ef36d3342af2c4d35b2
8GutiMu <you@example.com>
2017-10-29 04:34:46 -0600

    index base

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var plantilla = `  <div class="row">
+                    <div class="col s4 m4">
+                      <div class="card">
+                        <div class="card-image">
+                          <img src="images/sample-1.jpg">
+                          <span class="card-title">${title}</span>
+                        </div>
+                        <div class="card-content">
+                          <p>${content}</p>
+                        </div>
+                        <div class="card-action">
+                          <a href="#">${link}</a>
+                        </div>
+                      </div>
+                    </div>`
+
+document.ready(onload();)

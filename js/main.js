9c1338736fabf1f99ab4a62ad079bc819f5d759f
8GutiMu <you@example.com>
2017-10-29 06:36:20 -0600

    search page

--- js/main.js
+++ js/main.js
@@ -1,17 +1,47 @@
-var plantilla = `  <div class="row">
-                    <div class="col s4 m4">
+const apiExamples = [
+  {
+    title: 'Mundial 2022',
+    content: 'La Copa Mundial de la FIFA Rusia 2018 será la vigésima primera edición de la Copa Mundial de Fútbol.',
+    img: '../assets/img/rusia.jpg'
+  },
+  {
+    title: 'Cancún marzo 2017',
+    content: 'Viaja a Cancún al Mejor Precio para 2 personas, 5 dias 3 noches en un hotel 3 estrellas',
+    img: '../assets/img/cancun.jpg'
+  },
+  {
+    title: 'Maestría en finanzas ULA ',
+    content: 'MAestria de finanzas en la sede de la ULA, duración 2 años',
+    img: '../assets/img/graduado.jpg'
+  }
+]
+
+var loadPage = () => {
+  //code for iterate Api responses of most reachables.
+  var $reachableContainer = $("#reachables")
+
+  var result = apiExamples.map((prize) =>{
+  //  console.log(prize)
+  var template = `<div class="col s4 m4">
                       <div class="card">
                         <div class="card-image">
-                          <img src="images/sample-1.jpg">
-                          <span class="card-title">${title}</span>
+                          <img src=${prize.img}>
                         </div>
                         <div class="card-content">
-                          <p>${content}</p>
+                          <span class="card-title">${prize.title}</span>
+                          <p>${prize.content}</p>
                         </div>
                         <div class="card-action">
-                          <a href="#">${link}</a>
+                          <button href="#">Busca más</button>
                         </div>
-                      </div>
-                    </div>`
+                      </div>`
+    $reachableContainer.append(template)
+  })
+}
 
-document.ready(onload();)
+var nextLAnding = () => {
+  $(document).href = "search.html"
+}
+
+
+$(document).ready(loadPage);
